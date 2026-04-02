d0686a37ba192ab5ae0e66acd01bfa8a7b38f96d
cyyiiy <cyril.crozat@laposte.net>
2026-04-02 18:16:08 +0200

    [edit] reworked tokenizer tests
    they are now more consistant with other tests

--- compiler-tests/tests_tokenizer.cpp
+++ compiler-tests/tests_tokenizer.cpp
@@ -4,25 +4,73 @@
 
 TEST_SUITE_BEGIN("Tests Tokenizer");
 
-TEST_CASE("Tokenize Functional Code")
+TEST_CASE("Tokenize Exit Instruction")
 {
     // Setup
     // ===============================
-    const std::string input_code =
-        "const int value = 0;\n"
-        "exit(value);"
+    const std::string input_code = "exit(0);"
+    ;
+    const std::vector<token> desired_output = {
+        token(token_type::istr_exit),
+        token(token_type::sntx_par_open),
+        token(token_type::val_numeric, "0"),
+        token(token_type::sntx_par_close),
+        token(token_type::sntx_semicolon)
+    };
+    
+    // Execute
+    // ===============================
+    tokenizer tokenizer(input_code);
+    const std::vector<token> output_tokens = tokenizer.tokenize();
+    
+    // Verify
+    // ===============================
+    CHECK(output_tokens == desired_output);
+}
+
+TEST_CASE("Tokenize Print Instruction")
+{
+    // Setup
+    // ===============================
+    const std::string input_code = "print(\"Hello, World!\");"
+    ;
+    const std::vector<token> desired_output = {
+        token(token_type::istr_print),
+        token(token_type::sntx_par_open),
+        token(token_type::val_text, "Hello, World!"),
+        token(token_type::sntx_par_close),
+        token(token_type::sntx_semicolon)
+    };
+    
+    // Execute
+    // ===============================
+    tokenizer tokenizer(input_code);
+    const std::vector<token> output_tokens = tokenizer.tokenize();
+    
+    // Verify
+    // ===============================
+    CHECK(output_tokens == desired_output);
+}
+
+TEST_CASE("Tokenize Constant Declaration and Usage")
+{
+    // Setup
+    // ===============================
+    const std::string input_code = 
+        "const int constant = 0;\n"
+        "exit(constant);"
     ;
     const std::vector<token> desired_output = {
         token(token_type::istr_constant),
         token(token_type::type_int),
-        token(token_type::custom_name, "value"),
+        token(token_type::custom_name, "constant"),
         token(token_type::sntx_equal),
         token(token_type::val_numeric, "0"),
         token(token_type::sntx_semicolon),
         
         token(token_type::istr_exit),
         token(token_type::sntx_par_open),
-        token(token_type::custom_name, "value"),
+        token(token_type::custom_name, "constant"),
         token(token_type::sntx_par_close),
         token(token_type::sntx_semicolon)
     };
@@ -35,49 +83,14 @@ TEST_CASE("Tokenize Functional Code")
     // Verify
     // ===============================
     CHECK(output_tokens == desired_output);
-    
-}
-
-TEST_CASE("Tokenize Dysfunctional Code")
-{
-    // Setup
-    // ===============================
-    const std::string input_code =
-        "const string exit == 0\n"
-        "int)value(;"
-    ;
-    const std::vector<token> desired_output = {
-        token(token_type::istr_constant),
-        token(token_type::type_string),
-        token(token_type::istr_exit),
-        token(token_type::sntx_equal),
-        token(token_type::sntx_equal),
-        token(token_type::val_numeric, "0"),
-        
-        token(token_type::type_int),
-        token(token_type::sntx_par_close),
-        token(token_type::custom_name, "value"),
-        token(token_type::sntx_par_open),
-        token(token_type::sntx_semicolon)
-    };
-    
-    // Execute
-    // ===============================
-    tokenizer tokenizer(input_code);
-    const std::vector<token> output_tokens = tokenizer.tokenize();
-    
-    // Verify
-    // ===============================
-    CHECK(output_tokens == desired_output);
-    
 }
 
 TEST_CASE("Tokenize Unknown Syntax")
 {
     // Setup
     // ===============================
-    const std::string input_code = "exit(0); €";
     // Note: € symbol will never be added to the language
+    const std::string input_code = "exit(0); €";
     std::vector<token> output_tokens;
     
     // Execute
